bd05f9104bc3550795d672783659bdbc88e856e1
kotharigaurav <45046187+kotharigaurav@users.noreply.github.com>
2019-03-06 12:13:27 +0530

    Add files via upload

---
+++ primeno.sh
@@ -0,0 +1,17 @@
+
+echo "enter the number"
+read a
+i=2
+z=0
+while [ $i -lt $a ]
+do
+s=$((a%i))
+if [ $s -eq $z ]
+then
+echo "not prime"
+exit
+else
+i=`expr $i + 1`
+fi
+done 
+echo "prime number" 
